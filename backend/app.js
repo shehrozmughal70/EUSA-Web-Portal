c49a54f9d7012975dda132f797f76294d0cb84b4
maddog-1o1 <82176329+shehrozmughal70@users.noreply.github.com>
2021-04-10 22:33:03 +0500

    nodemailer module added

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var express = require('express');  
 var app = express();
+const bodyParser = require("body-parser");
 
 app.get('/', function (req, res) {  
    res.send('starting up the server');  
@@ -9,3 +10,6 @@ var host = server.address().address
   var port = server.address().port  
  console.log("Example app listening at http://%s:%s", host, port)  
 })  
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
--- backend/others/sendmail.js
+++ backend/others/sendmail.js
@@ -1,3 +1,25 @@
 const bodyParser = require("body-parser");
 const nodemailer = require('nodemailer');
 
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
+
+
+
+exports.sendEmail = (from, to, subject, text) => {
+    console.log("sendmail working");
+    transporter.sendMail({ from, to, subject, text }, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('EMail sent' + info.response);
+        }
+    })
+}
