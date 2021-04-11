e139a82a2c799cbdb606c838f33d8f4a1fe6c5d0
maddog-1o1 <82176329+shehrozmughal70@users.noreply.github.com>
2021-04-11 15:37:03 +0500

    Sending mail to the customer

--- backend/controllers/chatbot.js
+++ backend/controllers/chatbot.js
@@ -3,8 +3,11 @@ const Sndmail = require('../others/sendmail');
 
 exports.sendQuery = (req, res, next) => {
     const query = req.body.issue;
+    const msg='Thank you for your response, the support team is notified and will contact you'
 
-    Sndmail.sendEmail(query.email, 'fyp.binaryforce@gmail.com', 'Query', query.query);
+    Sndmail.sendEmail('fyp.binaryforce@gmail.com', 'fyp.binaryforce@gmail.com', 'Query', query.query+'. \nCustomer email is: '+query.email );
+    Sndmail.sendEmail('fyp.binaryforce@gmail.com', query.email, 'Customer Support', msg);
+
     res.status(200).json({ message: 'the support team is notified and will reach out to the customer' });
 
 }
